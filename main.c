67ca64cb59c8c4bff35d777895eaba3486963d93
ShejanMahamud <dev.shejanmahamud@gmail.com>
2025-09-18 14:55:43 +0600

    Refactor main function to implement a simple calculator that sums two user-input integers

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
 #include<stdio.h>
 
 int main (void) {
-    printf("Hello, World!\n");
+    int a,b;
+    printf("Welcome to calculator\n");
+    printf("Enter two numbers\n");
+    scanf("%d %d", &a, &b);
+    printf("The sum of %d and %d is %d\n", a, b, a+b);
     return 0;
 }
+/*
+format specifier
+%d => int
+%f => float
+%c => char
+%lld => long long int
+%lf = double(long float)
+*/
