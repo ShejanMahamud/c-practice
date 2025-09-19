62b00de8ce54184c943cc4635a207569c9a14d9b
ShejanMahamud <dev.shejanmahamud@gmail.com>
2025-09-19 10:33:54 +0600

    solved no.6 problems

--- codeforces/problem-6.c
+++ codeforces/problem-6.c
@@ -1,27 +1,29 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-    int a,b,c;
+int main() {
+    int a, b, c;
     scanf("%d %d %d", &a, &b, &c);
-        if (a <= b && a <= c )
-    {
-        printf("%d\n", a);
-    }else if (b <= a && b <= c)
-    {
-        printf("%d\n", b);
-    }else{
-        printf("%d\n", c);
-    }
-    if (a >= b && a >= c )
-    {
-        printf("%d\n", a);
-    }else if (b >= a && b >= c)
-    {
-        printf("%d\n", b);
-    }else{
-        printf("%d\n", c);
-    }
-    
-    
-  return 0;
+
+    int min, max;
+
+    // find min
+    if (a <= b && a <= c)
+        min = a;
+    else if (b <= a && b <= c)
+        min = b;
+    else
+        min = c;
+
+    // find max
+    if (a >= b && a >= c)
+        max = a;
+    else if (b >= a && b >= c)
+        max = b;
+    else
+        max = c;
+
+    // print in one line with space
+    printf("%d %d\n", min, max);
+
+    return 0;
 }
